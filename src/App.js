bc8a3357c45fa5717fe7076d80a091b5ea6c9ec6
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-04-16 20:14:03 +0530

    hef

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import HomePage from './pages/HomePage';
 
